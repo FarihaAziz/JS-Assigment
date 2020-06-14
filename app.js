0ff99950b778bdf9d807526af7bd2b9fff319ddd
FarihaAziz <64691106+FarihaAziz@users.noreply.github.com>
2020-06-14 05:24:01 -0700

    upload JS Assigment

---
+++ app.js
@@ -0,0 +1,133 @@
+// //  *********chapter no-1*******
+// //task 1.
+// alert("WELCOME TO  DREAMWORLD!");
+ 
+// //  task2.
+// alert ("Error! please enter a valid password.")
+// //task 3.
+
+// alert("Welcome to js land... \n Happy coding");
+// //task4.
+// alert("Welcome to js land...");
+
+// alert("Happy coding");
+// // task 5.
+// console.log("Hello i can run JS through my browser's console")
+
+// //  *********chapter no-2*******
+// // task1.
+// var username = "fariha";
+// // task2.
+// var myName = "fariha Aziz"
+// // task 3.
+//  var message = "Hello World"
+//  alert (message);
+
+// //  task 4.
+// //  task 4a.
+//  var studentName = "                  Jhone Doe                "
+//  alert(studentName);
+// // // task 4b.
+// var studentAge = "                 15 years old                 "
+//  alert(studentAge);
+// var qualification = "     Certified Mobile Application Developer     "
+//  alert(qualification);
+// // task 5.
+//  var foodItem ="       PIZZA \n     PIZZ\n     PIZ\n    PI\n   P       "
+//  alert (foodItem);
+// //   task 6.
+//  var email ="fariha_aziz_92@hotmail.com";
+//  alert(email);
+//    task 7.
+// var effort = "I am trying to learn from book ";
+// var book = "A smarter way to learn JavaScript";
+// var c = effort + book;
+// alert (c);
+// //    task 8.
+// var a ="yah! I can write HTML content through javascript";
+// document.write (a);
+        //   task  9.
+// var design ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ ";
+// alert(design);
+// ******** chapter No 3*********
+        //    task 1.
+// var age = "I am 18 years old";
+// alert (age);
+//          task 2.
+// var record = "you have visited this site 14 Times";
+// alert(record);
+//                task  3.
+// var birthYear = 1996;
+// document.write("My birthyear is"," "+birthYear);
+//            task 4.
+// var visitorName =prompt("Enter your name","john");
+// var productTitle =prompt("Enter product title","T-shirt");
+// var quantity =prompt("Enter Quanity","5");
+// document.write(visitorName+ " "+"orderd"+ " " +quantity + productTitle+" " + "on xyz store")
+//  ********** chapter no 5******
+//  ****task 1******
+//  
+// *****tsak 2******
+// *****5 legal variable***
+// userAlert
+// china$Rose
+// camelCow
+// myCountry
+// rose_Water
+// *****5 ILLlegal variable***
+// Alert
+// 3457
+// Var
+// air line 
+// "flyover"
+// ****** task 3******
+ 
+// document.write("<h1>Rules for naming JS variables</h1> <br/> Variable names can only contain ______, ______, ______ and ______. For example $my_1stVariable <br/> Variables must begin with a ______, ______ or _____. For example $name, _name or name <br/> Variable names are case _________  Variable names should not be JS" )
+// ********chapter no 5*****
+// ****** task 1****
+//  var a = 3;
+//  var b = 5;
+//  var c = a+b;
+//  document.write("Sum of"+ " " + a+ " " +"and"+ " " + b +" " +"is"+" " +  c)
+//  ****** task 2****
+//  *****for subtraction******
+//  var a = 12;
+//  var b = 5;
+//  var c = a-b;
+//  document.write("Subtraction of"+ " " + a+ " " +"and"+ " " + b +" " +"is"+" " +  c)
+//  *****for multiplication******
+//  var a = 12;
+//  var b = 4;
+//  var c = a*b;
+//  document.write("multiplication of"+ " " + a+ " " +"and"+ " " + b +" " +"is"+" " +  c)
+//  *****for division******
+//  var a = 12;
+//  var b = 3;
+//  var c = a/b;
+//  document.write("division of"+ " " + a+ " " +"and"+ " " + b +" " +"is"+" " +  c)
+//  *****for module******
+//  var a = 10;
+//  var b = 3;
+//  var c = a%b;
+//  document.write("module of"+ " " + a+ " " +"and"+ " " + b +" " +"is"+" " +  c)
+//   ********task 3*****
+     
+//    var a;
+//    document.write("Value after variable declaration is:",+ " "+a + "<br>");
+//     a =5;
+//    document.write("Initial value:",+ " "+a  + "<br>"  );
+   
+//     a = ++a;
+//    document.write("Value after increment is:",+ " "+a  + "<br>" );
+//     a = 7 + a;
+//    document.write("Value after addition is:",+ " "+a  + "<br>" );
+//    --a;
+//    document.write("Value after decrement is:",+ " "+a  + "<br>" );
+//    a=a%3;
+//    document.write("The reminder is:",+ " "+a  + "<br>" );
+//    **********task 4*********
+//    var ticketCost =600;
+//    ticketCost=5*600;
+//    document.write("Total cost to buy a movie is:",+ " "+ ticketCost+" "+"PKR" )
+   
+********** task 5 *********
